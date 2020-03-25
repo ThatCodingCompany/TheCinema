477f251e5eb3f49fb9fafa86fa5dd1c9274837af
Banson Xu <xzcxzcyy@126.com>
2020-03-25 23:28:48 +0800

    test#1 add Permission

--- app/src/main/java/com/thatcodingcompany/theCinema/MainActivity.java
+++ app/src/main/java/com/thatcodingcompany/theCinema/MainActivity.java
@@ -1,8 +1,13 @@
 package com.thatcodingcompany.theCinema;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkOrRequestPermission();
+
+    }
+
+    public boolean checkOrRequestPermission() {
+        String[] PERMISSIONS_STORAGE = {
+                "android.permission.CAMERA",
+                "android.permission.RECORD_AUDIO"};
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, "android.permission.CAMERA") != PackageManager.PERMISSION_GRANTED
+                    || ContextCompat.checkSelfPermission(this, "android.permission.RECORD_AUDIO") != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(PERMISSIONS_STORAGE, 101);
+                return false;
+            }
+        }
+        return true;
     }
 }
